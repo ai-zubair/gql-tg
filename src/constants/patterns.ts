2e99a62098e5a2d9c7af8839929920f5dc0110b0
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-06 20:20:42 +0530

    Refactor pattern constants

---
+++ src/constants/patterns.ts
@@ -0,0 +1,17 @@
+import { ROOT_OP_NAMES, GQL_NAMED_TYPES } from '../types';
+
+export const DEF_GEN_PATTERN = `\\n(?=\\s*${GQL_NAMED_TYPES.OBJECT_TYPE}|\\s*${GQL_NAMED_TYPES.INPUT}|\\s*${GQL_NAMED_TYPES.ENUM}|\\s*${GQL_NAMED_TYPES.INTERFACE}|\\s*${GQL_NAMED_TYPES.UNION})`;
+
+export const QUERY_OP_PATTERN = `\\b${ROOT_OP_NAMES.QUERY}(?=\\s*{)`;
+
+export const MUTATION_OP_PATTERN = `\\b${ROOT_OP_NAMES.MUTATION}(?=\\s*{)`;
+
+export const SUBSCRIPTION_OP_PATTERN = `\\b${ROOT_OP_NAMES.SUBSCRIPTION}(?=\\s*{)`;
+
+export const ROOT_OP_PATTERN = `${QUERY_OP_PATTERN}|${MUTATION_OP_PATTERN}|${SUBSCRIPTION_OP_PATTERN}`;
+
+export const CHARAC_CLEAN_PATTERN = `\\s+|${GQL_NAMED_TYPES.OBJECT_TYPE}|{|}`;
+
+export const EMPTY_STRING_PATTERN = `^$`;
+
+export const ER_SIGNATURE_SPLIT_PATTERN = `(?<=\\)|^\\w+):`;
