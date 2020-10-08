ae8183daf49c46d11e869f5275e3198272c3977c
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-08 15:54:33 +0530

    Integrate parsing changes into defGen

--- src/components/generator/DefinitionGenerator.ts
+++ src/components/generator/DefinitionGenerator.ts
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import { GQLRootOperationTupleMap, GQLRootOperationMap, GQLRootOperation, GQLExecutionRequest, ExecutionRequestArg, ArgTuple, ExecutionRequestReturn, ROOT_OP_NAMES, GQL_INPUT_TYPES, GQL_OUTPUT_TYPES, GQLschemaParser } from '../../types';
-import { CHARAC_CLEAN_PATTERN, EMPTY_STRING_PATTERN, ER_SPLIT_PATTERN, SIGNATURE_SPLIT_PATTERN, ARG_SPLIT_PATTERN, REQUIRED_ARG_PATTERN } from '../../constants';
+import { EMPTY_STRING_PATTERN, ER_SPLIT_PATTERN, SIGNATURE_SPLIT_PATTERN, ARG_SPLIT_PATTERN, REQUIRED_ARG_PATTERN } from '../../constants';
 import { SchemaParser } from '../parser/SchemaParser';
 
 class DefinitionGenerator {
@@ -16,16 +16,14 @@ class DefinitionGenerator {
     if(this.schemaParser.rootOperationDefinitions.length === 0){
       throw new Error("No root operation type defintions found in the schema file!"); 
     }
-    const charCleanPattern = new RegExp(CHARAC_CLEAN_PATTERN, 'gi');
-    const characterCleanedRootDefintions = this.schemaParser.rootOperationDefinitions.map( (defString):string => defString.replace(charCleanPattern,''));
     const defTupleMap: GQLRootOperationTupleMap = {
       [ROOT_OP_NAMES.QUERY]: undefined,
       [ROOT_OP_NAMES.MUTATION]: undefined,
       [ROOT_OP_NAMES.SUBSCRIPTION]: undefined
     }
-    characterCleanedRootDefintions.forEach( rootDefString => {
-      console.log(rootDefString);
+    this.schemaParser.rootOperationDefinitions.forEach( rootDefString => {
       let rootDefTuple = rootDefString.split(this.schemaParser.parsingDelimiter);
+      rootDefTuple.shift();
       const rootOpName = rootDefTuple.shift().toUpperCase();
       const emptyAttributePattern = new RegExp(EMPTY_STRING_PATTERN);
       rootDefTuple = rootDefTuple.filter( execRequest => !emptyAttributePattern.test(execRequest) );
