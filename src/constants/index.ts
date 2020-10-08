0e921777acbb92dc9588d95dbf00b3fe6f3acc4b
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-08 20:44:06 +0530

    Add verbose type definitions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,1 @@
-export * from './patterns';
 export * from './parsing';
--- src/types/parserTypes.ts
+++ src/types/parserTypes.ts
@@ -4,9 +4,21 @@ export interface GQLNamedTypeMap{
   [GQLtypeLabel: string]: GQL_NAMED_TYPES | GQL_INPUT_TYPES | GQL_OUTPUT_TYPES;
 }
 
+/**
+ * @type TokenizedTypeDefinition =>
+ * GQL Type definition string tokenized  with the delimiter :: in the form 
+ * GQL_NAMED_TYPE::TYPE_LABEL::TYPE_FIELD_1::TYPE_FIELD_2::TYPE_FIELD_3
+ * 
+ * e.g. type::Query::posts(keyword:String):[Posts!]!::users:[Users!]!
+ * 
+ * e.g. input::UserCreateArgs::email:String!::password:String!
+ */
+
+export type TokenizedTypeDefinition = string;
+
 export interface GQLschemaParser{
-  typeDefinitions: string[];
-  rootOperationDefinitions: string[];
+  typeDefinitions: TokenizedTypeDefinition[];
+  rootOperationDefinitions: TokenizedTypeDefinition[];
   namedTypeMap: GQLNamedTypeMap;
   parsingDelimiter: string;
 }
