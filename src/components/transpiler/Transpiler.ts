9ac2196847cdca6aad53f16e84751b90ed39a4a6
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-24 19:16:46 +0530

    Constants refactoring

--- src/components/transpiler/Transpiler.ts
+++ src/components/transpiler/Transpiler.ts
@@ -20,16 +20,24 @@ import {
   ARG_SUFFIX,
   LIST_SUFFIX,
   INDENT_SPACE,
-  EXPORT,
   NEW_LINE,
-  UNION_BAR
+  UNION_BAR,
+  INCORRECT_OUTPUT_FILE_NAME_ERROR,
+  DEF_FILE_EXTENSION,
+  DEFAULT_SCHEMA_PATH,
+  DEFAULT_DEFINITIONS_PATH,
+  INCORRECT_OUTPUT_FILE_PATH_ERROR
 } from '../../constants';
 
 import { 
-  CAMEL_CASE_PATTERN, 
-  FIRST_LETTER_PATTERN, 
   UNION_BAR_PATTERN 
 } from '../../patterns'
+
+import { 
+  formatIntoTranspiledTypeDefinition,
+  formatIntoTranspiledFieldDefinition,
+  joinIntoCamelCase
+} from './utils';
 
 import { Parser } from "../parser/Parser";
 
@@ -40,12 +48,12 @@ class Transpiler {
   private transpiledDefinitions = 0;
   public definitionsPath: string;
 
-  constructor(schemaPath: string = './schema.graphql', definitionsPath: string = './definitions.ts', customParser?:GQLschemaParser) {
+  constructor(schemaPath: string = DEFAULT_SCHEMA_PATH, definitionsPath: string = DEFAULT_DEFINITIONS_PATH, customParser?:GQLschemaParser) {
     this.schemaParser = customParser || new Parser(schemaPath);
     this.parsedSchema = this.schemaParser.parsedSchema;
     this.definitionsPath = resolve(process.cwd(),definitionsPath);
-    if(!this.definitionsPath.endsWith('.ts')){
-      throw new Error("Output file must be a TS file.");
+    if(!this.definitionsPath.endsWith(DEF_FILE_EXTENSION)){
+      throw new Error(INCORRECT_OUTPUT_FILE_NAME_ERROR);
     }
   }
 
@@ -67,7 +75,7 @@ class Transpiler {
     try{
       writeFileSync(this.definitionsPath,this.transpiledSchema);
     }catch(err){
-      throw new Error("Invalid Output File Path!");
+      throw new Error(INCORRECT_OUTPUT_FILE_PATH_ERROR);
     }
     process.stdout.write(`[SUCCESS]: ${this.transpiledDefinitions} definitions written to ${this.definitionsPath} in ${(timeTakenToWriteDefinitions).toFixed(2)}ms\n`);
   }
@@ -81,7 +89,7 @@ class Transpiler {
 
   private transpileParsedExecutionRequest(executionRequest: GQLExecutionRequest, rootOperationName: string): string{
     if(executionRequest.requestArgs > 0){
-      const transpiledReqArgsLabel = this.joinIntoCamelCase(executionRequest.requestName, rootOperationName, ARG_SUFFIX);
+      const transpiledReqArgsLabel = joinIntoCamelCase(executionRequest.requestName, rootOperationName, ARG_SUFFIX);
       return this.transpileParsedExecReqArg(executionRequest.requestArgDefs, transpiledReqArgsLabel);
     }
   }
@@ -89,7 +97,7 @@ class Transpiler {
   private transpileParsedExecReqArg(execReqArgs: ExecRequestArg[], transpiledReqArgsLabel: string): string{
     const transpiledArgList = execReqArgs.map(this.transpileGenericField);
     const transpiledArgs = transpiledArgList.join('');
-    const transpiledArgDef = this.formatIntoTranspiledTypeDefinition(transpiledReqArgsLabel, transpiledArgs, MAPPED_NON_SCALARS.INTERFACE);
+    const transpiledArgDef = formatIntoTranspiledTypeDefinition(transpiledReqArgsLabel, transpiledArgs, MAPPED_NON_SCALARS.INTERFACE);
     return transpiledArgDef;
   }
 
@@ -99,7 +107,7 @@ class Transpiler {
     const nonScalarReturnType = fieldType.nonScalarTypeName;
     const listWrappingType = fieldType.isList ? LIST_SUFFIX : '';
     const mappedType = scalarReturnType ? `${TRANSPILED_SCALARS_MAP[scalarReturnType]}${listWrappingType}` : `${nonScalarReturnType}${listWrappingType}`;
-    return this.formatIntoTranspiledFieldDefinition(fieldLabel, mappedType, fieldType.isOptional);
+    return formatIntoTranspiledFieldDefinition(fieldLabel, mappedType, fieldType.isOptional);
   }
 
   private transpileParsedNonScalar(nonScalarTypeName: string){
@@ -109,7 +117,7 @@ class Transpiler {
     const transpiledFieldList = nonScalarType.typeFields.map(nonScalarFieldTranspiler);
     const terminalBarPattern =  new RegExp(UNION_BAR_PATTERN);
     const transpiledFields = transpiledFieldList.join('').replace(terminalBarPattern,'');
-    const transpiledFieldDef = this.formatIntoTranspiledTypeDefinition(nonScalarTypeName, transpiledFields, mappedNonScalarType);
+    const transpiledFieldDef = formatIntoTranspiledTypeDefinition(nonScalarTypeName, transpiledFields, mappedNonScalarType);
     return transpiledFieldDef;
   }
 
@@ -122,34 +130,6 @@ class Transpiler {
       default:
         return this.transpileGenericField;
     }
-  }
-
-  private formatIntoTranspiledTypeDefinition(defLabel: string, defTypes: string, typeName: string): string{
-    switch(typeName){
-      case MAPPED_NON_SCALARS.TYPE:
-        return `${NEW_LINE}${EXPORT} ${typeName} ${defLabel} = ${defTypes};${NEW_LINE}`;    
-      default:
-        return `${NEW_LINE}${EXPORT} ${typeName} ${defLabel}{${NEW_LINE}${defTypes}}${NEW_LINE}`;
-    }
-  }
-
-  private formatIntoTranspiledFieldDefinition(fieldName: string, fieldType: string, isOptional: boolean): string{
-    return isOptional ? `${INDENT_SPACE}${fieldName}?: ${fieldType};${NEW_LINE}` : `${INDENT_SPACE}${fieldName}: ${fieldType};${NEW_LINE}`;
-  }
-
-  private joinIntoCamelCase(...str: string[]): string {
-    const titleCasedStrings = str.map((str: string):string => {
-      const lowerCasedString = this.hasCamelCasing(str) ? str : str.toLowerCase();
-      const firstLetterPattern = new RegExp(FIRST_LETTER_PATTERN);
-      const camedlCasedString = lowerCasedString.replace(firstLetterPattern,str[0].toUpperCase());
-      return camedlCasedString;
-    });
-    return titleCasedStrings.join('');
-  }
-
-  private hasCamelCasing(str: string): boolean{
-    const camelCasePattern = new RegExp(CAMEL_CASE_PATTERN);
-    return camelCasePattern.test(str);
   }
 }
 
